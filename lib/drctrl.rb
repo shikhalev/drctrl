2860a8a8acf3f5769801de01ef01e2380b38cb0a
Ivan Shikhalev <shikhalev@gmail.com>
2015-03-29 16:39:03 +0500

    Build #0.1.0.23

--- lib/drctrl.rb
+++ lib/drctrl.rb
@@ -1,7 +1,5 @@
 # encoding: utf-8
 
-# require 'pp'
-
 require 'drb'
 
 require_relative 'drctrl/version'
@@ -59,7 +57,6 @@ module DRCtrl
       end
       oldprim = DRb.primary_server
       DRb.start_service uri, Server.new(&block), opts
-      # pp prim: DRb.primary_server, oldprim: oldprim
       DRb.primary_server = oldprim if oldprim
     end
 
